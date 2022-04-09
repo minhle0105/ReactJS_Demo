0fec27ceaaf8a8d7647dae4542988fc99391ee40
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-04-09 16:06:36 -0400

    State example

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import './App.css';
+import {useState} from "react";
 import {TonTable} from "./TonTable";
+
 function App() {
 
     // html duoc return trong component luon phai co 1 html element outermost
 
-    const title = "TEN NHA TON"
+    // day 2: state / props
+    const [number, setNumber] = useState(0);
+    const [showTable, setShowTable] = useState(false);
+
+    const handleDecrease = () => {
+        setNumber(number - 1);
+    }
+
+    const handleIncrease = () => {
+        setNumber(number + 1);
+    }
+
+    const handleToggle = () => {
+        const newShowTable = !showTable;
+        setShowTable(newShowTable);
+    }
+
+    const title = "COUNTER APP"
 
     return (
         <div className="App">
             <h1>{title}</h1>
-            <label htmlFor="name">Name</label>
-            <input id="name" type="text"/>
-            <TonTable />
+            <button onClick={handleDecrease}>-</button>
+            <h2>{number}</h2>
+            <button onClick={handleIncrease}>+</button>
+            <button onClick={handleToggle}>{showTable ? "Hide" : "Show"}</button>
+            {showTable ? <TonTable /> : null}
         </div>
     );
 }
